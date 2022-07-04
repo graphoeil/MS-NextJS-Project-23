486e51edf5a2dc7b34ed8dc0b097f8e7d855e074
graphoeil <graphoeil@gmail.com>
2022-07-04 17:12:19 +0200

    MongoDB network access comment

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout/Layout";
 
 // Deploying app with Vercel.com via Github, 
 // then no need to build, start ... it's continous integration ,-)
+// Warning in MongoDB "Network Access" make sure to let access 
+// from anywhere with ip : 0.0.0.0/0  (includes your current IP address)
 
 // App component => root component
 function MyApp({ Component, pageProps }) {
